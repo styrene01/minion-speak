2f9f7221ffca5713335dbdc0730932bad7412df9
styrene <waghelaharsh56@gmail.com>
2022-07-22 16:36:14 +0530

    exercise 3

---
+++ app.js
@@ -0,0 +1,6 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+function clickEvent() {
+    console.log("Button is clicked");
+}
+btnTranslate.addEventListener("click", clickEvent);
